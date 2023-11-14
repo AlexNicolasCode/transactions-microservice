94f6d5da92f2f9e25a05ad9cb1a1229b628899a2
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-14 17:17:22 -0300

    feat: Controller interface was created

---
+++ src/presentation/protocol/controller.py
@@ -0,0 +1,13 @@
+from abc import ABCMeta, abstractmethod
+from typing import TypeVar, Generic
+
+from presentation.protocol.http import HttpResponse
+from presentation.protocol.http import HttpRequest
+
+R = TypeVar("R")
+
+class Controller(Generic[R], metaclass=ABCMeta):
+    
+    @abstractmethod
+    async def handle(self, request: HttpRequest) -> HttpResponse[R]:
+        pass
