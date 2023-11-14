b3a4de592790e532d5eab596e49d19b3b198cc72
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-14 17:15:45 -0300

    feat: change str to float type to avoid throw when client side send value field on request

--- src/domain/model/income.py
+++ src/domain/model/income.py
@@ -6,7 +6,7 @@ class Income:
     id: str
     user_id: str
     account_id: str
-    value: str
+    value: float
     is_ignored: bool
     created_at: datetime.datetime
     updated_at: datetime.datetime
