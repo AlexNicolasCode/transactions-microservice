e4c69fa76a1bef7ba4fc58e4a8ce7baa7c1b0ccf
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-14 15:58:13 -0300

    feat: IncomeMongoRepository was created to enable manipulate database IncomeEntity

---
+++ src/infra/database/repository/income_mongo_repository.py
@@ -0,0 +1,15 @@
+from domain.model.income import Income
+from data.protocol.database.save_income_repository import SaveIncomeRepository
+from infra.database.repository import IncomeEntity
+
+class IncomeMongoRepository(SaveIncomeRepository):
+    async def save_income(self, income: Income) -> bool:
+        try:
+            await IncomeEntity(
+                account_id=income.account_id,
+                user_id=income.user_id,
+                value=income.value,
+            ).save()
+            return True
+        except:
+            return False
