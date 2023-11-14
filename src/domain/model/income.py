e327289c47c1fd5cababac5bb008228ef80e00b7
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-14 14:07:20 -0300

    feat: income model was created

---
+++ src/domain/model/income.py
@@ -0,0 +1,12 @@
+import datetime
+from dataclasses import dataclass
+
+@dataclass
+class Income:
+    id: str
+    user_id: str
+    account_id: str
+    value: str
+    is_ignored: bool
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
