632e497bbfce7565c8f429afde783906c58cf3d0
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-14 17:30:13 -0300

    feat: SaveIncomeController was created

---
+++ src/presentation/controller/save_income_controller.py
@@ -0,0 +1,17 @@
+from presentation.protocol.controller import Controller
+from presentation.protocol.http import HttpResponse, HttpRequest
+from presentation.helper.http_helper import created, serverError
+from domain.usecase.save_income import SaveIncome
+from domain.dto.save_income_dto import SaveIncomeDTO
+
+class SaveIncomeController(Controller[str]):
+
+    def __init__(self, save_income_external: SaveIncome):
+        self.save_income = save_income_external
+
+    async def handle(self, request: HttpRequest[SaveIncomeDTO]) -> HttpResponse[bool]:
+        hasSaved = await self.save_income.save_income(request.body)
+        if hasSaved:
+            return created()
+        else:
+            return serverError()
