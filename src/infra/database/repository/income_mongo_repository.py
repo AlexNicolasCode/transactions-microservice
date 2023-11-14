210559286e134e0799248928f50f3e7d7cadc780
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-14 17:14:05 -0300

    fix: replace import IncomeEntity path to enable use it entity on flow

--- src/infra/database/repository/income_mongo_repository.py
+++ src/infra/database/repository/income_mongo_repository.py
@@ -1,6 +1,6 @@
 from domain.model.income import Income
 from data.protocol.database.save_income_repository import SaveIncomeRepository
-from infra.database.repository import IncomeEntity
+from infra.database.entity.income_entity import IncomeEntity
 
 class IncomeMongoRepository(SaveIncomeRepository):
     async def save_income(self, income: Income) -> bool:
