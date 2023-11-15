80c4e527718cc9a197b2f29d04e515476ef7a38a
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-14 22:25:39 -0300

    feat: DbLoadIncomes was created

---
+++ src/data/usecase/db_load_incomes.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from domain.model.income import Income
+from domain.usecase.save_income import SaveIncome
+from data.protocol.database.load_incomes_repository import LoadIncomesRepository
+
+class DbSaveIncome(SaveIncome):
+    def __init__(self, load_incomes_repository: LoadIncomesRepository) -> None:
+        self.repository = load_incomes_repository
+
+    async def load_incomes(self, user_id: str) -> List[Income]:
+        return await self.repository.load_incomes(user_id=user_id)
