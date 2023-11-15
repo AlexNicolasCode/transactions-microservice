1ffe379890ffb53e9e66dae67bc3a3b7a3f8eaee
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-15 11:42:09 -0300

    feat: load incomes was settled

--- src/main/route/income_routes.py
+++ src/main/route/income_routes.py
@@ -1,12 +1,29 @@
-from fastapi import APIRouter, Response
+import jwt
+from fastapi import APIRouter, Response, Depends
+from fastapi.security import OAuth2PasswordBearer
 
 from domain.dto.save_income_dto import SaveIncomeDTO
+from domain.dto.load_incomes_dto import LoadIncomesDTO
 from presentation.protocol.http import HttpRequest
 from main.factory.controller.save_income_controller_factory import makeSaveIncomeController
+from main.factory.controller.load_incomes_controller_factory import makeLoadIncomesController
 
 income_router = APIRouter(
     prefix="/income"
 )
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@income_router.get("/")
+async def load(token: str = Depends(oauth2_scheme)) -> list:
+    decoded_token = jwt.decode(token, options={"verify_signature": False})
+    request = HttpRequest[LoadIncomesDTO]()
+    request.body = {"user_id": decoded_token["user_id"]}
+    controller = makeLoadIncomesController()
+    response = await controller.handle(request)
+    if response["status_code"] == 200:
+        return response["body"]
+    return Response(status_code = 500)
 
 @income_router.post("/")
 async def save(income: SaveIncomeDTO) -> None:
