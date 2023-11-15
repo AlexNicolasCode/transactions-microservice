e835a9c99a6309fe260afc62609a6add4c0478d8
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-15 12:20:55 -0300

    feat: route adapter was created to provide ecosystem to controller

---
+++ src/main/adapter/route_adapter.py
@@ -0,0 +1,23 @@
+from typing import TypeVar
+
+import jwt
+from fastapi import Response
+
+from presentation.protocol.http import HttpRequest
+from presentation.protocol.controller import Controller
+
+T = TypeVar("T")
+
+async def route_adapter(controller: Controller, body: T, token: str) -> Response:
+    request = HttpRequest()
+    request.body = body
+    if token != None:
+        decoded_token = jwt.decode(token, options={"verify_signature": False})
+        request.body["user_id"] = decoded_token["user_id"]
+    response = await controller.handle(request)
+    status_mapper = {
+        200: response["body"],
+        201: None,
+        500: Response(status_code=500),
+    }
+    return status_mapper.get(response["status_code"], 500)
