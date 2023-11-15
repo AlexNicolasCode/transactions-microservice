feba8d542711460cc2c138cc96848ef524c5c252
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-15 12:21:06 -0300

    feat: apply route adapter

--- src/main/route/income_routes.py
+++ src/main/route/income_routes.py
@@ -3,10 +3,9 @@ from fastapi import APIRouter, Response, Depends
 from fastapi.security import OAuth2PasswordBearer
 
 from domain.dto.save_income_dto import SaveIncomeDTO
-from domain.dto.load_incomes_dto import LoadIncomesDTO
-from presentation.protocol.http import HttpRequest
 from main.factory.controller.save_income_controller_factory import makeSaveIncomeController
 from main.factory.controller.load_incomes_controller_factory import makeLoadIncomesController
+from main.adapter.route_adapter import route_adapter
 
 income_router = APIRouter(
     prefix="/income"
@@ -16,22 +15,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @income_router.get("/")
 async def load(token: str = Depends(oauth2_scheme)) -> list:
-    decoded_token = jwt.decode(token, options={"verify_signature": False})
-    request = HttpRequest[LoadIncomesDTO]()
-    request.body = {"user_id": decoded_token["user_id"]}
-    controller = makeLoadIncomesController()
-    response = await controller.handle(request)
-    if response["status_code"] == 200:
-        return response["body"]
-    return Response(status_code = 500)
+    return await route_adapter(makeLoadIncomesController(), {}, token)
 
 @income_router.post("/")
 async def save(income: SaveIncomeDTO) -> None:
-    request = HttpRequest[SaveIncomeDTO]()
-    request.body = income
-    controller = makeSaveIncomeController()
-    response = await controller.handle(request)
-    return Response(
-        status_code=response["status_code"],
-        content=response["body"],
-    )
+    return await route_adapter(makeSaveIncomeController(), income, None)
