355287c1083128ffd56fa954ec4a9312d9ea3653
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-14 22:02:29 -0300

    feat: initial Fast API setup was settled

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Response
+from contextlib import asynccontextmanager
+from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+
+from infra.database.entity.income_entity import IncomeEntity
+from main.route.income_routes import income_router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    client = AsyncIOMotorClient("mongodb://test:test@localhost:27017")
+    await init_beanie(database=client.db_name, document_models=[IncomeEntity])
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(income_router)
+
+@app.get("/")
+async def root() -> None:
+    return Response(status_code=404)
+
