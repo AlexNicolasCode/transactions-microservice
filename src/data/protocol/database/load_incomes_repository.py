eeccdbd1d30590b6108ea013934b06ee27455100
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-14 22:23:51 -0300

    feat: LoadIncomesRepository interface was created

---
+++ src/data/protocol/database/load_incomes_repository.py
@@ -0,0 +1,9 @@
+from typing import List
+from abc import ABCMeta, abstractmethod
+
+from domain.model.income import Income
+
+class LoadIncomesRepository(metaclass=ABCMeta):
+    @abstractmethod
+    def save_income(self, user_id: str) -> List[Income]:
+        pass
